3bb0c2bf573488c8a738af0653a175b9e537ba1e
Sergei Shekshuev <sergei.shekshuev@gmail.com>
2022-11-03 12:16:14 +0300

    .env database configuration added

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,19 +4,21 @@ import { AppService } from "./app.service";
 import { AccountsModule } from "./accounts/accounts.module";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { ConfigModule } from "@nestjs/config";
+
 @Module({
     imports: [
-        TypeOrmModule.forRoot({
-            type: "postgres",
-            host: "localhost",
-            port: 5432,
-            username: "postgres",
-            password: "postgres",
-            database: "projectzero",
-            autoLoadEntities: true,
-            synchronize: true
-        }),
         ConfigModule.forRoot(),
+        TypeOrmModule.forRoot({
+            type: (process.env.DATABASE_TYPE as any) || "sqlite",
+            database: process.env.DATABASE_NAME || "database.db",
+            autoLoadEntities: true,
+            logging: true,
+            synchronize: true,
+            host: process.env.DATABASE_HOST,
+            port: Number(process.env.DATABASE_PORT),
+            username: process.env.DATABASE_USERNAME,
+            password: process.env.DATABASE_PASSWORD
+        }),
         AccountsModule
     ],
     controllers: [AppController],
